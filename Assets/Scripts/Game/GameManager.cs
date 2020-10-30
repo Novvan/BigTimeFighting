7e63dd75c15956527376ee231fd079ee3aee2b28
novvan <ianallengeier@gmail.com>
2020-10-30 16:12:56 -0300

    (feat)Character Spawn

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _player1;
+    private GameObject _ia;
+    void Start()
+    {
+        _player1 = Instantiate(_player1, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
