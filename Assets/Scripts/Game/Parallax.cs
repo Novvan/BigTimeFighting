20fd2a038054e5021f8e7fd648286cead0eb9bf1
Federico Gaudino <fede.gaudino@hotmail.com>
2020-11-08 01:03:35 -0300

    Level 2 progress

--- Assets/Scripts/Game/Parallax.cs
+++ Assets/Scripts/Game/Parallax.cs
@@ -7,7 +7,7 @@ public class Parallax: MonoBehaviour {
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
-    private float lenght;
+    // private float lenght;
 
     void Start() {
         startpos = transform.position.x;
@@ -15,13 +15,13 @@ public class Parallax: MonoBehaviour {
     }
 
     void FixedUpdate() {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        // float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
         
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - lenght) startpos -= length;
+        // if (temp > startpos + length) startpos += length;
+        // else if (temp < startpos - lenght) startpos -= length;
 
     }
 }
